4bc583854717cd961b7bcf0045b9387bf16b9372
carera <jardaminarik@gmail.com>
2024-01-01 21:50:49 +1100

    Chage folder struct for easier .gitignore time

--- parse.js
+++ parse.js
@@ -4,5 +4,5 @@ const filename = process.argv[2];
 
 const file = fs.readFileSync(filename, "utf-8");
 const data = JSON5.parse(file);
-fs.writeFileSync("./heap-data-example.json", JSON.stringify(data), "utf-8");
+fs.writeFileSync("./data/heap-data-example.json", JSON.stringify(data), "utf-8");
 // console.log(JSON.stringify(data, null, 2));
